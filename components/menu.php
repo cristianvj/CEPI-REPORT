7bc1fbc5e6771776ae6e63c2f900b8ff17b3cdb8
Cristian Villota J <cristiandavid.vj@gmail.com>
2017-10-05 18:12:34 -0500

    agregamos los enlaces al menú

--- components/menu.php
+++ components/menu.php
@@ -22,55 +22,55 @@
 		</a>
 	</div>
 	<div class="div-item" id="item-outlook">
-		<a href="#">
+		<a href="<?php echo $ruta ?>templates/outlook">
 			<img id="icon-outlook" src="<?php echo $ruta ?>img/public/icons-menu/outlook.png" alt="">
 			2050 Outlook
 		</a>
 	</div>
 	<div class="div-item" id="item-policy">
-		<a href="#">
+		<a href="">
 			<img id="icon-policy" src="<?php echo $ruta ?>img/public/icons-menu/policy.png" alt="">
 			Policy Blocks
 		</a>
 	</div>
 	<div class="div-item" id="item-forest">
-		<a href="#" class="div-item-child">
+		<a href="<?php echo $ruta ?>templates/forest" class="div-item-child">
 			<img id="icon-forest" src="<?php echo $ruta ?>img/public/icons-menu/forest.png" alt="">
 			Forest
 		</a>
 	</div>
 	<div class="div-item" id="item-bioeconomy">
-		<a href="#" class="div-item-child">
+		<a href="<?php echo $ruta ?>templates/bioeconomy" class="div-item-child">
 			<img id="icon-bioeconomy" src="<?php echo $ruta ?>img/public/icons-menu/bioeconomy.png" alt="">
 			Bioeconomy and Innovation
 		</a>
 	</div>
 	<div class="div-item" id="item-circular">
-		<a href="#" class="div-item-child">
+		<a href="<?php echo $ruta ?>templates/circular" class="div-item-child">
 			<img id="icon-circular" src="<?php echo $ruta ?>img/public/icons-menu/circular.png" alt="">
 			Circular Economy
 		</a>
 	</div>
 	<div class="div-item" id="item-energy">
-		<a href="#" class="div-item-child">
+		<a href="<?php echo $ruta ?>templates/energy" class="div-item-child">
 			<img id="icon-energy" src="<?php echo $ruta ?>img/public/icons-menu/energy.png" alt="">
 			Energy and Climate
 		</a>
 	</div>
 	<div class="div-item" id="item-enviroment">
-		<a href="#" class="div-item-child">
+		<a href="<?php echo $ruta ?>templates/environment" class="div-item-child">
 			<img id="icon-enviroment" src="<?php echo $ruta ?>img/public/icons-menu/enviroment.png" alt="">
-			Enviroment
+			Environment
 		</a>
 	</div>
 	<div class="div-item" id="item-latest">
-		<a href="#">
+		<a href="<?php echo $ruta ?>templates/latest">
 			<img id="icon-latest" src="<?php echo $ruta ?>img/public/icons-menu/latest.png" alt="">
 			All Latest Data
 		</a>
 	</div>
 	<div class="div-item" id="item-glossary">
-		<a href="#">
+		<a href="<?php echo $ruta ?>templates/glossary">
 			<img id="icon-glossary" src="<?php echo $ruta ?>img/public/icons-menu/glossary.png" alt="">
 			Glossary
 		</a>
