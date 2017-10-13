8a1fc94ddc61ec2ce6f286379b37ac890c8bad73
Cristian Villota J <cristiandavid.vj@gmail.com>
2017-10-13 12:16:05 -0500

    function charts

--- templates/latest/js/graficas.js
+++ templates/latest/js/graficas.js
@@ -1,39 +1,45 @@
-$(function() { 
-    var myChart = Highcharts.chart('key-figures', {
-        chart: {
-            type: 'bar'
-        },
-        title: {
-            text: 'Fruit Consumption'
-        },
-        xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
-        },
-        yAxis: {
-            title: {
-                text: 'Fruit eaten'
-            }
-        },
-        series: [{
-            name: 'Jane',
-            data: [1, 0, 4]
-        }, {
-            name: 'John',
-            data: [5, 7, 3]
-        }]
-    });
-});
 $(document).ready(function(){
+	function chart1(){
+		var myChart = Highcharts.chart('key-figures', {
+		    chart: {
+		        type: 'bar'
+		    },
+		    title: {
+		        text: 'Fruit Consumption'
+		    },
+		    xAxis: {
+		        categories: ['Apples', 'Bananas', 'Oranges']
+		    },
+		    yAxis: {
+		        title: {
+		            text: 'Fruit eaten'
+		        }
+		    },
+		    series: [{
+		        name: 'Jane',
+		        data: [1, 0, 4]
+		    }, {
+		        name: 'John',
+		        data: [5, 7, 3]
+		    }]
+		});
+	}
+	chart1();
    $('#charts').change(function(){
 	    var valor = $(this).val();
 		if(valor=="key-figures"){
 			$(function() { 
-			    var myChart = Highcharts.chart('key-figures', {
+			    chart1();
+			});
+		}
+		if(valor=="1"){
+			$(function() { 
+			    var myChart2 = Highcharts.chart('key-figures', {
 			        chart: {
 			            type: 'bar'
 			        },
 			        title: {
-			            text: 'Fruit Consumption'
+			            text: 'ee'
 			        },
 			        xAxis: {
 			            categories: ['Apples', 'Bananas', 'Oranges']
@@ -53,14 +59,14 @@ $(document).ready(function(){
 			    });
 			});
 		}
-		if(valor=="1"){
+		if(valor=="2"){
 			$(function() { 
-			    var myChart = Highcharts.chart('key-figures', {
+			    var myChart2 = Highcharts.chart('key-figures', {
 			        chart: {
 			            type: 'bar'
 			        },
 			        title: {
-			            text: 'Fruit Consumption'
+			            text: 'chart 2'
 			        },
 			        xAxis: {
 			            categories: ['Apples', 'Bananas', 'Oranges']
