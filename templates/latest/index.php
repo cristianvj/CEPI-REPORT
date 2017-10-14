59c6daf2dcbd1cede5bf86c036c3d326305b23dc
Cristian David <cristian@Cristians-MacBook-Air.local>
2017-10-13 22:56:17 -0500

    crear objeto para la tabla

--- templates/latest/index.php
+++ templates/latest/index.php
@@ -30,142 +30,91 @@
 				</ul>
 				<!-- /Botones -->
 				<div class="tab-content flex contBusqueda">
+
 					<!-- Tab1 -->
-					<div id="about" class="tab active">
-						
+					<div id="about" class="tab active">						
 							<div class="caja">
-								<select id="charts" name="charts" class="charts">
-								<option value="key-figures">Key Figures</option>
-								<option value="1">Direct manufacturing cost structure of the European Pulp & Paper Industry</option>
-								<option value="2" >Profitability European Pulp & Paper Industry</option>
-								<option value="3">Investment / turnover ratio in the Pulp & Paper Industry, CEPI countries</option>
-								<option value="4">Labour productivity: comparison between Europe and competing countries</option>
-								<option value="5">Evolution of P&B Production by Region</option>
-								<option>P&B Consumption per Capita</option>
-								<option>CEPI Paper & Board Production by Grade</option>
+								<select id="chart-about" name="chart-about" class="charts">
+								<option value="1">Key Figures</option>
+								<option value="2">Direct manufacturing cost structure of the European Pulp & Paper Industry</option>
+								<option value="3" >Profitability European Pulp & Paper Industry</option>
+								<option value="4">Investment / turnover ratio in the Pulp & Paper Industry, CEPI countries</option>
+								<option value="5">Labour productivity: comparison between Europe and competing countries</option>
+								<option value="6">Evolution of P&B Production by Region</option>
+								<option value="7">P&B Consumption per Capita</option>
+								<option value="8">CEPI Paper & Board Production by Grade</option>
 							</select>
-							</div>
-						
+							</div>						
 					</div>
 					<!-- /Tab1 -->
+
 					<!-- Tab2 -->
 					<div id="wood" class="tab">
-						<div class="cont_select_center">
-						<div class="select_mate" data-mate-select="active" >
-							<select name="" onclick="return false;" id="">
-								<option value="">CEPI Wood Consumption Origin</option>
-								<option value="1">CEPI Trade Flows of Wood</option>
+						<div class="caja" >
+							<select id="chart-wood" name="chart-wood" class="charts">
+								<option value="1">CEPI Wood Consumption Origin</option>
+								<option value="2">CEPI Trade Flows of Wood</option>
 							</select>
-							<p class="selecionado_opcion"  onclick="open_select(this)" ></p><span onclick="open_select(this)" class="icon_select_mate" ><svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-							    <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/>
-							    <path d="M0-.75h24v24H0z" fill="none"/>
-							</svg></span>
-							<div class="cont_list_select_mate">
-							  <ul class="cont_select_int">  </ul> 
-							</div>
-						  </div>
 						</div>
 					</div>
 					<!-- /Tab2 -->
+
 					<!-- Tab3 -->
 					<div id="raw" class="tab">
-						<div class="cont_select_center">
-						<div class="select_mate" data-mate-select="active" >
-							<select name="" onclick="return false;" id="">
-								<option value="">CEPI Raw Materials Consumption</option>
-								<option value="1">CEPI Non-Fibrous Materials Consumption</option>
-							</select>
-							<p class="selecionado_opcion"  onclick="open_select(this)" ></p><span onclick="open_select(this)" class="icon_select_mate" ><svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-							    <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/>
-							    <path d="M0-.75h24v24H0z" fill="none"/>
-							</svg></span>
-							<div class="cont_list_select_mate">
-							  <ul class="cont_select_int">  </ul> 
-							</div>
-						  </div>
+						<div class="caja">						
+							<select id="chart-raw" name="chart-raw" class="charts">
+								<option value="1">CEPI Raw Materials Consumption</option>
+								<option value="2">CEPI Non-Fibrous Materials Consumption</option>
+							</select>							
 						</div>
 					</div>
 					<!-- /Tab3 -->
+
 					<!-- Tab4 -->
 					<div id="paper" class="tab">
-						<div class="cont_select_center">
-						<div class="select_mate" data-mate-select="active" >
-							<select name="" onclick="return false;" id="">
-								<option value="">European Recycling Rate of Paper for Recycling</option>
-								<option value="1">European Recycling Rate in the World</option>
+						<div class="caja">
+							<select id="chart-paper" name="chart-paper" class="charts">
+								<option value="1">European Recycling Rate of Paper for Recycling</option>
+								<option value="2">European Recycling Rate in the World</option>
 							</select>
-							<p class="selecionado_opcion"  onclick="open_select(this)" ></p><span onclick="open_select(this)" class="icon_select_mate" ><svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-							    <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/>
-							    <path d="M0-.75h24v24H0z" fill="none"/>
-							</svg></span>
-							<div class="cont_list_select_mate">
-							  <ul class="cont_select_int">  </ul> 
-							</div>
-						  </div>
 						</div>
 					</div>
 					<!-- /Tab4 -->
+
 					<!-- Tab5 -->
 					<div id="energy" class="tab">
-						<div class="cont_select_center">
-						<div class="select_mate" data-mate-select="active" >
-							<select name="" onclick="return false;" id="">
-								<option value="">Total Specific Energy Consumption</option>
-								<option value="1">Specific Electricity Consumption</option>
+						<div class="caja">
+							<select id="chart-energy" name="chart-energy" class="charts">
+								<option value="1">Total Specific Energy Consumption</option>
+								<option value="2">Specific Electricity Consumption</option>
 								<option>Electricity produced through CHP compared to Total on-site Electricity Generation</option>
 								<option>Evolution of Energy Consumption</option>
 								<option>Biomass Utilisation - Fuel Consumption</option>
 							</select>
-							<p class="selecionado_opcion"  onclick="open_select(this)" ></p><span onclick="open_select(this)" class="icon_select_mate" ><svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-							    <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/>
-							    <path d="M0-.75h24v24H0z" fill="none"/>
-							</svg></span>
-							<div class="cont_list_select_mate">
-							  <ul class="cont_select_int">  </ul> 
-							</div>
-						  </div>
 						</div>
 					</div>
 					<!-- /Tab5 -->
 					<!-- Tab6 -->
 					<div id="environment" class="tab">
-						<div class="cont_select_center">
-						<div class="select_mate" data-mate-select="active" >
-							<select name="" onclick="return false;" id="">
-								<option value="">Evolution of Environmental Impacts of the CEPI Pulp and Paper Industry</option>
-								<option value="1">Water Profile (Sankey Diagram)</option>
-								<option>Biomass Utilisation - Fuel Consumption</option>
-								<option>Specific Air Emissions</option>
-								<option>Specific Amount of Residues Landfilled</option>
-								<option>Water Emissions</option>
-								<option>Residues by Destination in 2015</option>
+						<div class="caja">
+							<select id="chart-environment" name="chart-environment" class="charts">
+								<option value="1">Evolution of Environmental Impacts of the CEPI Pulp and Paper Industry</option>
+								<option value="2">Water Profile (Sankey Diagram)</option>
+								<option value="3">Biomass Utilisation - Fuel Consumption</option>
+								<option value="4">Specific Air Emissions</option>
+								<option value="5">Specific Amount of Residues Landfilled</option>
+								<option value="6">Water Emissions</option>
+								<option value="7">Residues by Destination in 2015</option>
 							</select>
-							<p class="selecionado_opcion"  onclick="open_select(this)" ></p><span onclick="open_select(this)" class="icon_select_mate" ><svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-							    <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/>
-							    <path d="M0-.75h24v24H0z" fill="none"/>
-							</svg></span>
-							<div class="cont_list_select_mate">
-							  <ul class="cont_select_int">  </ul> 
-							</div>
-						  </div>
 						</div>
 					</div>
 					<!-- /Tab6 -->
 					<!-- Tab7 -->
 					<div id="social" class="tab">
-						<div class="cont_select_center">
-						<div class="select_mate" data-mate-select="active" >
-							<select name="" onclick="return false;" id="">
-								<option value="">Accident Rate</option>
+						<div class="caja">
+							<select id="chart-social" name="chart-social" class="charts">
+								<option value="1"> Accident Rate </option>
 							</select>
-							<p class="selecionado_opcion"  onclick="open_select(this)" ></p><span onclick="open_select(this)" class="icon_select_mate" ><svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-							    <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/>
-							    <path d="M0-.75h24v24H0z" fill="none"/>
-							</svg></span>
-							<div class="cont_list_select_mate">
-							  <ul class="cont_select_int">  </ul> 
-							</div>
-						  </div>
 						</div>
 					</div>
 					<!-- /Tab7 -->
--- templates/latest/js/graficas.js
+++ templates/latest/js/graficas.js
@@ -1,5 +1,163 @@
 $(document).ready(function(){
-	function chart1(){
+
+	keyFigures();
+
+   $('#chart-about').change(function(){
+	    var valor = $(this).val();
+		if(valor=="1"){
+			$(function() { 
+			    keyFigures();
+			});
+		}
+		if(valor=="2"){
+			$(function() { 
+			    directManufacturing();
+			});
+		}
+		if(valor=="3"){
+			$(function() { 
+			    keyFigures();
+			});
+		}
+		if(valor=="4"){
+			$(function() { 
+			    keyFigures();
+			});
+		}
+		if(valor=="5"){
+			$(function() { 
+			    keyFigures();
+			});
+		}
+		if(valor=="6"){
+			$(function() { 
+			    keyFigures();
+			});
+		}
+		if(valor=="7"){
+			$(function() { 
+			    keyFigures();
+			});
+		}
+		if(valor=="8"){
+			$(function() { 
+			    keyFigures();
+			});
+		}
+	});
+
+   //Functions charts
+
+   function keyFigures(){
+   	
+   	var dataTable = [
+   		{
+   			"name":"Turnover (Million €)", 
+	   		"data": 
+	   		{
+   				"a": "79,388",
+   				"b": "74,537",
+   				"c": "75,790",
+   				"d": "78,437",
+   				"e": "80,241",
+	   		}
+   		},
+   		{
+   			"name":"P&B Production (000 tonnes)",
+	   		"data":
+	   		{
+	   			"a": "90,823",
+	   			"b": "98,259",
+	   			"c": "95,065",
+	   			"d": "91,019",
+	   			"e": "90,872",
+	   		},	
+   		},	
+   		{
+   			"name":"Market Pulp (000 tonnes)",
+	   		"data":
+	   		{
+	   			"a": "11,423",
+	   			"b": "13,142",
+	   			"c": "12,706",
+	   			"d": "13,174",
+	   			"e": "13,087",
+	   		},	
+   		},
+   		{
+   			"name":"Share of global production",
+	   		"data":
+	   		{
+	   			"a": "27.7%",
+	   			"b": "26.8%",
+	   			"c": "24.1%",
+	   			"d": "22.4%",
+	   			"e": "22.3%",
+	   		},	
+   		},
+   		{
+   			"name":"Companies",
+	   		"data":
+	   		{
+	   			"a": "929",
+	   			"b": "831",
+	   			"c": "674",
+	   			"d": "628",
+	   			"e": "633",
+	   		},	
+   		},
+   		{
+   			"name":"Mills",
+	   		"data":
+	   		{
+	   			"a": "1,309",
+	   			"b": "1,224",
+	   			"c": "992",
+	   			"d": "919",
+	   			"e": "917",
+	   		},	
+   		},
+   		{
+   			"name":"Employment",
+	   		"data":
+	   		{
+	   			"a": "279,987",
+	   			"b": "246,785",
+	   			"c": "194,894",
+	   			"d": "181,084",
+	   			"e": "178,212",
+	   		},	
+   		},
+   		{
+   			"name":"Added Value (Million €)",
+	   		"data":
+	   		{
+	   			"a": "24,494",
+	   			"b": "18,154",
+	   			"c": "16,560",
+	   			"d": "16,684",
+	   			"e": "16,500",
+	   		},	
+   		},
+   		{
+   			"name":"Investments (Million €)",
+	   		"data":
+	   		{
+	   			"a": "5,637",
+	   			"b": "5,318",
+	   			"c": "2,913",
+	   			"d": "3,245",
+	   			"e": "3,500",
+	   		},	
+   		},
+   	];
+
+
+
+   	alert(dataTable[0].data.a);
+   }
+
+   function directManufacturing(){
 		var myChart = Highcharts.chart('key-figures', {
 		    chart: {
 		        type: 'bar'
@@ -24,68 +182,5 @@ $(document).ready(function(){
 		    }]
 		});
 	}
-	chart1();
-   $('#charts').change(function(){
-	    var valor = $(this).val();
-		if(valor=="key-figures"){
-			$(function() { 
-			    chart1();
-			});
-		}
-		if(valor=="1"){
-			$(function() { 
-			    var myChart2 = Highcharts.chart('key-figures', {
-			        chart: {
-			            type: 'bar'
-			        },
-			        title: {
-			            text: 'ee'
-			        },
-			        xAxis: {
-			            categories: ['Apples', 'Bananas', 'Oranges']
-			        },
-			        yAxis: {
-			            title: {
-			                text: 'Fruit eaten'
-			            }
-			        },
-			        series: [{
-			            name: 'Jane',
-			            data: [1, 0, 4]
-			        }, {
-			            name: 'John',
-			            data: [5, 7, 3]
-			        }]
-			    });
-			});
-		}
-		if(valor=="2"){
-			$(function() { 
-			    var myChart2 = Highcharts.chart('key-figures', {
-			        chart: {
-			            type: 'bar'
-			        },
-			        title: {
-			            text: 'chart 2'
-			        },
-			        xAxis: {
-			            categories: ['Apples', 'Bananas', 'Oranges']
-			        },
-			        yAxis: {
-			            title: {
-			                text: 'Fruit eaten'
-			            }
-			        },
-			        series: [{
-			            name: 'Jane',
-			            data: [1, 0, 4]
-			        }, {
-			            name: 'John',
-			            data: [5, 7, 3]
-			        }]
-			    });
-			});
-		}
-	});
 });
 
